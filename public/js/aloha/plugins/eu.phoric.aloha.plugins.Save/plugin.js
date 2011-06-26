f1f59634d10d1cd3a55f37a830c00f97c47432e2
a.j.delaney <a.j.delaney@brighton.ac.uk>
2011-06-26 23:50:34 +0000

    Checking in Aloha deps

---
+++ public/js/aloha/plugins/eu.phoric.aloha.plugins.Save/plugin.js
@@ -0,0 +1,18 @@
+if(typeof EXAMPLE=="undefined"||!EXAMPLE)
+{
+  var EXAMPLE={}
+}
+
+EXAMPLE.DummySavePlugin=new GENTICS.Aloha.Plugin("eu.phoric.aloha.plugins.Save");
+EXAMPLE.DummySavePlugin.languages=["en"];
+
+EXAMPLE.DummySavePlugin.init=function(){
+  var that=this;
+  var saveButton=new GENTICS.Aloha.ui.Button({label:this.i18n("save"),onclick:function(){that.save()}});
+
+    GENTICS.Aloha.FloatingMenu.addButton('GENTICS.Aloha.global', saveButton, 'Format', 0)};
+  EXAMPLE.DummySavePlugin.save=function(){
+   var content="";
+   jQuery.each(GENTICS.Aloha.editables,function(index,editable){content=content+"Editable ID: "+editable.getId()+"\nHTML code: "+editable.getContents()+"\n\n"});
+   alert(this.i18n("saveMessage")+"\n\n"+content)
+  };
